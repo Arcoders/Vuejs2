54ccd116889b031a5b96c023e625d3bf5e926364
arcoders <anonismaa@gmail.com>
2018-01-08 22:35:17 +0100

    Vuex structure

--- vue-vuex/src/store/store.js
+++ vue-vuex/src/store/store.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import productos from './modules/productos';
+import {getters} from './getters';
 
 Vue.use(Vuex);
 
@@ -11,17 +12,13 @@ export const store = new Vuex.Store({
        carro: []
    },
 
-    getters: {
-        totalCompra: state => state.carro.reduce((total, producto) => total + producto.precio, 0)
-    },
+    getters: getters,
 
     mutations: {
         comprarProducto: (state, indice) => state.carro.unshift(state.productos[indice]),
         eliminarProducto: (state, indice) => state.carro.splice(indice, 1)
     },
 
-    modules: {
-       productos
-   }
+    modules: { productos }
 
 });
